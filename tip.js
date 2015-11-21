56d5e8bd032399249ab73ecd82d847c14f85ec46
Travis Infield <travisinfield@gmail.com>
2015-11-21 04:56:29 -0500

    refactored js and included bootstrap

--- tip.js
+++ tip.js
@@ -1,31 +1,34 @@
 var bill = document.getElementById('bill');
-var percentOptions = document.getElementsByName('percent-box')[0];
 var button = document.getElementsByName('button') [0];
-var customerPercentInput = document.getElementById('cust-percent');
+var percent = 0;
+var btnPercents = document.getElementsByClassName('btn-default');
+var customPercent = document.getElementById('custom');
 
-var customerPercentSelected = percentOptions.addEventListener('change', function () {
-  var classList = customerPercentInput.classList;
-  if(percentOptions[percentOptions.selectedIndex].value === 'custom'){
-    classList.remove('cust-percent-hide');
-    classList.add('cust-percent-show');
-  } else {
-    classList.add('cust-percent-hide');
-    classList.remove('cust-percent-show');
-  }
+var setPercent = function (value) {
+  customPercent.value = value.replace('%', '');  percent = Number(value.replace('%', '')).toFixed(2) / 100;
+};
+
+btnPercents[0].addEventListener('click', function (e) {
+  setPercent(this.innerHTML);
+  calculate();
+});
+btnPercents[1].addEventListener('click', function (e) {
+  setPercent(this.innerHTML);
+  calculate();
+});
+customPercent.addEventListener('keyup', function (e) {
+  setPercent(this.value);
+  calculate();
+});
+bill.addEventListener('keyup', function () {
+  calculate();
 });
 
-var calculate = button.addEventListener('click', function () {
+var calculate = function () {
   var formattedBill = Number(bill.value).toFixed(2);
-  var percent;
-
-  if(percentOptions[percentOptions.selectedIndex].value === 'custom'){
-    percent = Number(document.getElementById('custom').value).toFixed(2) / 100;
-  } else {
-    percent = percentOptions[percentOptions.selectedIndex].value / 100;
-  }
-
   var tip = Number(formattedBill * percent).toFixed(2);
   var total = Number(formattedBill) + Number(tip);
+  console.log(formattedBill, tip, total);
   document.getElementById('tip-amt').innerHTML = 'Tip: $' + tip;
   document.getElementById('total-amt').innerHTML = 'Total $' + total.toFixed(2);
-});
+};
